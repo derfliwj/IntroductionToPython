291719b4bfb2f7284d9d840714d9a49f1a67eb34
derfliwj <derfliwj@rose-hulman.edu>
2017-11-27 15:21:04 -0500

    DONE

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Wesley Derflinger.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -27,4 +27,39 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #  fixing them at either this session OR at the NEXT session.
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+green_turtle = rg.SimpleTurtle('turtle')
+green_turtle.pen = rg.Pen('green', 3)
+green_turtle.speed = 1
+
+wes_turtle = rg.SimpleTurtle('turtle')
+wes_turtle.pen = rg.Pen('orange', 5)
+wes_turtle.speed = 5
+
+size_1 = 300
+size_2 = 150
+
+for k in range(5):
+
+    green_turtle.draw_square(size_1)
+    wes_turtle.draw_square(size_2)
+    green_turtle.pen_up()
+    wes_turtle.pen_up()
+    green_turtle.backward(45)
+    wes_turtle.forward(45)
+    green_turtle.forward(10)
+    wes_turtle.backward(10)
+    green_turtle.left(45)
+    wes_turtle.right(45)
+    green_turtle.pen_down()
+    wes_turtle.pen_down()
+    size_1 = size_1 - 12
+    size_2 = size_2 - 5
+
+
+window.close_on_mouse_click()
 ########################################################################
